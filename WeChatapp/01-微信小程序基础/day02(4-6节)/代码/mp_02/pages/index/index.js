378b2988404bc7a19ac8c06b3b07ba747a93783a
rttmoa <rttmoa@gmail.com>
2023-02-11 14:36:34 +0800

    Wechat

--- WeChatapp/01-微信小程序基础/day02(4-6节)/代码/mp_02/pages/index/index.js
+++ WeChatapp/01-微信小程序基础/day02(4-6节)/代码/mp_02/pages/index/index.js
@@ -18,6 +18,8 @@ Page({
     msg: '你好，',
     type: 1,
     flag: true,
+
+    // wx:for 渲染数据
     arr1: ['苹果', '华为', '小米'],
     userList: [
       { id: 1, name: '小红' },
@@ -27,30 +29,24 @@ Page({
   },
 
   // 定义按钮的事件处理函数
-  btnTapHandler(e) {  //-->  事件对象！
-    console.log(e)  //-->  当事件回调触发的时候、会收到一个事件对象event、属性如：type、timeStamp、target、currentTarget、detail、touches、changedTouches
+  btnTapHandler(event) {  //-->  事件对象！
+    console.log(event)  //-->  当事件回调触发的时候、会收到一个事件对象event、属性如：type、timeStamp、target、currentTarget、detail、touches、changedTouches
                   //-->  target和currentTarget区别：target是触发该事件的源头组件、而currentTarget则是当前事件所绑定的组件
   },
 
   // +1 按钮的点击事件处理函数
   CountChange() {
-    this.setData({
-      count: this.data.count + 1
-    })
+    this.setData({ count: this.data.count + 1 })
   },
 
   btnTap2(e) {
-    this.setData({
-      count: this.data.count + e.target.dataset.info
-    })
+    this.setData({ count: this.data.count + e.target.dataset.info }) // 获取自定义属性 data-info
   },
 
-  // input 输入框的事件处理函数
+  // input 输入框的事件处理函数   ->  文本框与data数据之间的同步
   inputHandler(e) {
     // console.log(e.detail.value)
-    this.setData({
-      msg: e.detail.value
-    })
+    this.setData({ msg: e.detail.value })
   },
 
 
