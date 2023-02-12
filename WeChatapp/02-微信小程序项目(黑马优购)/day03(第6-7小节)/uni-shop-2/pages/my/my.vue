f405461028e9d943cf5e214c4d765c3f32a30f87
rttmoa <rttmoa@gmail.com>
2023-02-13 06:02:06 +0800

    WeChat

---
+++ WeChatapp/02-微信小程序项目(黑马优购)/day03(第6-7小节)/uni-shop-2/pages/my/my.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    My
+  </view>
+</template>
+
+<script>
+  import badgeMix from '@/mixins/tabbar-badge.js'
+
+  export default {
+    mixins: [badgeMix],
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ WeChatapp/02-微信小程序项目(黑马优购)/day03(第6-7小节)/uni-shop-2/store/cart.js
@@ -0,0 +1,35 @@
+export default {
+  namespaced: true,
+
+  state: () => ({
+    // 购物车的数组，用来存储购物车中每个商品的信息对象
+    // 每个商品的信息对象，都包含如下 6 个属性：
+    // { goods_id, goods_name, goods_price, goods_count, goods_small_logo, goods_state }
+    cart: JSON.parse(uni.getStorageSync('cart') || '[]')
+  }),
+
+  mutations: {
+    addToCart(state, goods) {
+      const findResult = state.cart.find(x => x.goods_id === goods.goods_id)
+
+      if (!findResult) {
+        state.cart.push(goods)
+      } else {
+        findResult.goods_count++
+      }
+      
+      this.commit('m_cart/saveToStorage')
+    },
+    saveToStorage(state) {
+      uni.setStorageSync('cart', JSON.stringify(state.cart))
+    }
+  },
+
+  getters: {
+    total(state) {
+      let c = 0
+      state.cart.forEach(x => c += x.goods_count)
+      return c
+    }
+  }
+}
