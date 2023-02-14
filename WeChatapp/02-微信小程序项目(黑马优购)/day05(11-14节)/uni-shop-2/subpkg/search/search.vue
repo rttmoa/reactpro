69fd11d3de66b336988833f1532327b6281dd0cd
rttmoa <rttmoa@gmail.com>
2023-02-14 13:02:16 +0800

    1

--- WeChatapp/02-微信小程序项目(黑马优购)/day05(11-14节)/uni-shop-2/subpkg/search/search.vue
+++ WeChatapp/02-微信小程序项目(黑马优购)/day05(11-14节)/uni-shop-2/subpkg/search/search.vue
@@ -1,8 +1,14 @@
 <template>
   <view>
+
+    <!-- search-box  吸顶效果： -->
+    <!-- uni-search-bar文档： https://uniapp.dcloud.net.cn/component/uniui/uni-search-bar.html# -->
     <view class="search-box">
-      <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
+    <!-- 监听input输入的、官方文档组件的使用、隐藏右侧取消按钮 -->
+      <uni-search-bar @input="input" :radius="100" cancelButton="none">
+      </uni-search-bar>
     </view>
+
 
     <!-- 搜索建议列表 -->
     <view class="sugg-list" v-if="searchResults.length !== 0">
@@ -12,26 +18,41 @@
       </view>
     </view>
 
-    <!-- 搜索历史 -->
+
+    <!-- 搜索历史(历史记录跳转) -->
     <view class="history-box" v-else>
+
       <!-- 标题区域 -->
       <view class="history-title">
         <text>搜索历史</text>
         <uni-icons type="trash" size="17" @click="clean"></uni-icons>
       </view>
+
       <!-- 列表区域 -->
       <view class="history-list">
+      <!-- 跳转到商品列表页面：gotoGoodsList -->
         <uni-tag :text="item" v-for="(item, i) in histories" :key="i" @click="gotoGoodsList(item)"></uni-tag>
       </view>
+
     </view>
+
   </view>
 </template>
+
+
+<!-- 
+  此节知识点：
+    1、
+ -->
+
 
 <script>
   export default {
     data() {
       return {
+        // 延时器的timerId
         timer: null,
+        // 搜索关键词
         kw: '',
         // 搜索的结果列表
         searchResults: [],
@@ -40,14 +61,16 @@
       };
     },
     onLoad() {
-      this.historyList = JSON.parse(uni.getStorageSync('kw') || '[]')
+      this.historyList = JSON.parse(uni.getStorageSync('kw') || '[]');  // 如果前面的值不存在 取[]
     },
     methods: {
       // input 输入事件的处理函数
       input(e) {
+        // 清除timer对应的延时器
         clearTimeout(this.timer)
-
+        // 重新启动一个延时器、并把timerId赋值给this.timer
         this.timer = setTimeout(() => {
+          // 如果500毫秒内、没有触发新的输入事件，则为搜索关键词赋值
           this.kw = e.value
           this.getSearchList()
         }, 500)
@@ -71,7 +94,7 @@
         })
       },
       saveSearchHistory() {
-        // this.historyList.push(this.kw)
+        // this.historyList.push(this.kw) // 不建议：1、新关键词应该放到首位， 2、关键词不应该重复
 
         const set = new Set(this.historyList)
         set.delete(this.kw)
@@ -86,14 +109,17 @@
         this.historyList = []
         uni.setStorageSync('kw', '[]')
       },
+      // 历史记录跳转到商品详情页中去
       gotoGoodsList(kw) {
         uni.navigateTo({
-          url: '/subpkg/goods_list/goods_list?query=' + kw
+          url: '/subpkg/goods_list/goods_list?query=' + kw, // 跳转到商品列表页同时 携带query参数
         })
       }
     },
+
     computed: {
       histories() {
+        // console.log(123)
         return [...this.historyList].reverse()
       }
     }
@@ -127,11 +153,14 @@
     }
   }
 
+
+  // 搜索历史盒子
   .history-box {
     padding: 0 5px;
 
     .history-title {
       display: flex;
+      // 两端对齐
       justify-content: space-between;
       height: 40px;
       align-items: center;
@@ -143,6 +172,7 @@
       display: flex;
       flex-wrap: wrap;
 
+      // 标签名就是类名：审查元素
       .uni-tag {
         margin-top: 5px;
         margin-right: 5px;
