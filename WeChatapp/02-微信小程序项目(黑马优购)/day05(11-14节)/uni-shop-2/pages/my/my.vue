04beb8c0f0e93a50b2ca5e5ba0ba40d925f94848
rttmoa <rttmoa@gmail.com>
2023-02-15 00:04:00 +0800

    1

--- WeChatapp/02-微信小程序项目(黑马优购)/day05(11-14节)/uni-shop-2/pages/my/my.vue
+++ WeChatapp/02-微信小程序项目(黑马优购)/day05(11-14节)/uni-shop-2/pages/my/my.vue
@@ -1,5 +1,6 @@
 <template>
   <view class="my-container">
+	  <!-- 这里不让取反先进入页面查看页面结构 -->
     <my-login v-if="!token"></my-login>
 
     <my-userinfo v-else></my-userinfo>
@@ -14,8 +15,7 @@
     mixins: [badgeMix],
     data() {
       return {
-
-      };
+      };	
     },
     computed: {
       ...mapState('m_user', ['token'])
@@ -24,6 +24,8 @@
 </script>
 
 <style lang="scss">
+
+  // 用户信息页面需要 100%高度的灰色
   page,
   .my-container {
     height: 100%;
--- WeChatapp/02-微信小程序项目(黑马优购)/day05(11-14节)/uni-shop-2/store/store.js
+++ WeChatapp/02-微信小程序项目(黑马优购)/day05(11-14节)/uni-shop-2/store/store.js
@@ -3,7 +3,12 @@ import Vuex from 'vuex'
 import moduleCart from '@/store/cart.js'
 import moduleUser from '@/store/user.js'
 
+
 Vue.use(Vuex)
+
+
+
+
 
 const store = new Vuex.Store({
   modules: {
