a9618a664b2c0fdc573389c50cd1c1c6e47dcbd2
rttmoa <rttmoa@gmail.com>
2023-09-22 12:50:19 +0800

    -

---
+++ HooksAdmin/src/views/dashboard/dash/embedded/index.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable prettier/prettier */
+import "./index.less";
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Embedded = () => {
+	console.log("===========iframe组件嵌入式=============");
+	return <iframe src="https://cn.bing.com/" frameBorder="0" className="card full-iframe"></iframe>;
+};
+
+export default Embedded;
---
+++ HooksAdmin/src/views/dataScreen/components/DataHeaderTime.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { useTimes } from "@/hooks/useTime";
+
+
+
+
+
+
+
+
+const DataHeaderTime = () => {
+	const { time } = useTimes();
+
+	return <span className="header-time">当前时间：{time}</span>;
+};
+
+export default DataHeaderTime;
